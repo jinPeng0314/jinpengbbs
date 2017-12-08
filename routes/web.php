78f434698625232758a8879fde06ed9c7b7701b2
zhangjinpeng <zhangjinpeng@easy2study.net>
2017-12-08 21:47:24 +0800

    新增功能：用户上传话题图片

--- routes/web.php
+++ routes/web.php
@@ -36,3 +36,6 @@ Route::resource('topics', 'TopicsController', ['only' => ['index', 'show', 'crea
 
 //分类
 Route::resource('categories', 'CategoriesController', ['only' => ['show']]);
+
+//图片上传
+Route::post('upload_image', 'TopicsController@uploadImage')->name('topics.upload_image');
