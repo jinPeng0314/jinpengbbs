79a2142a247bda6a047e35fb7f474038195f9974
zhangjinpeng <zhangjinpeng@easy2study.net>
2017-12-15 15:59:12 +0800

    新增功能：用户创建回复

--- app/Http/Requests/ReplyRequest.php
+++ app/Http/Requests/ReplyRequest.php
@@ -6,36 +6,16 @@ class ReplyRequest extends Request
 {
     public function rules()
     {
-        switch($this->method())
-        {
-            // CREATE
-            case 'POST':
-            {
-                return [
-                    // CREATE ROLES
-                ];
-            }
-            // UPDATE
-            case 'PUT':
-            case 'PATCH':
-            {
-                return [
-                    // UPDATE ROLES
-                ];
-            }
-            case 'GET':
-            case 'DELETE':
-            default:
-            {
-                return [];
-            };
-        }
+        return [
+            'content' => 'required|min:2',
+        ];
     }
 
     public function messages()
     {
         return [
-            // Validation messages
+            'content.required' => '回复内容不能为空!',
+            'content.min' => '回复内容至少有2个字符!',
         ];
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -42,4 +42,6 @@ Route::resource('categories', 'CategoriesController', ['only' => ['show']]);
 
 //图片上传
 Route::post('upload_image', 'TopicsController@uploadImage')->name('topics.upload_image');
-Route::resource('replies', 'RepliesController', ['only' => ['index', 'show', 'create', 'store', 'update', 'edit', 'destroy']]);
+
+//话题回复
+Route::resource('replies', 'RepliesController', ['only' => ['store','destroy']]);
