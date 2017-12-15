76b41f4f42fa4643cb8dbb03976c63e95ae4adc6
zhangjinpeng <zhangjinpeng@easy2study.net>
2017-12-15 15:11:45 +0800

    新增功能：用户回复列表

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -11,12 +11,12 @@
                 <div class="panel-body">
                     <div class="media">
                         <div align="center">
-                            <img class="thumbnail img-responsive" src="{{ $user->avatar }}" width="300px" height="300px">
+                            <img class="thumbnail img-responsive" src="{{ isset($user->avatar) ? $user->avatar : '/images/default.png' }}" width="300px" height="300px">
                         </div>
                         <div class="media-body">
                             <hr>
                             <h4><strong>个人简介</strong></h4>
-                            <p>{{ $user->introduction }}</p>
+                            <p>{{ isset($user->introduction) ? $user->introduction : '暂无~_~' }}</p>
                             <hr>
                             <h4><strong>注册于</strong></h4>
                             <p>{{ $user->created_at->diffForHumans() }}</p>
@@ -39,10 +39,18 @@
             <div class="panel panel-default">
                 <div class="panel-body">
                     <ul class="nav nav-tabs">
-                        <li class="active"><a href="#">Ta 的话题</a></li>
-                        <li><a href="#">Ta 的回复</a></li>
+                        <li class="{{ active_class(if_query('tab', null)) }}">
+                            <a href="{{ route('users.show', $user->id) }}">Ta 的话题</a>
+                        </li>
+                        <li class="{{ active_class(if_query('tab', 'replies')) }}">
+                            <a href="{{ route('users.show', [$user->id, 'tab' => 'replies']) }}">Ta 的回复</a>
+                        </li>
                     </ul>
-                    @include('users._topics', ['topics' => $user->topics()->recent()->paginate(5)])
+                    @if (if_query('tab', 'replies'))
+                        @include('users._replies', ['replies' => $user->replies()->with('topic')->recent()->paginate(5)])
+                    @else
+                        @include('users._topics', ['topics' => $user->topics()->recent()->paginate(5)])
+                    @endif
                 </div>
             </div>
 
--- routes/web.php
+++ routes/web.php
@@ -42,3 +42,4 @@ Route::resource('categories', 'CategoriesController', ['only' => ['show']]);
 
 //图片上传
 Route::post('upload_image', 'TopicsController@uploadImage')->name('topics.upload_image');
+Route::resource('replies', 'RepliesController', ['only' => ['index', 'show', 'create', 'store', 'update', 'edit', 'destroy']]);
