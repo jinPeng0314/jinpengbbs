3f01b300c0a8ffa91585ebb7b9b23b2598c88a90
zhangjinpeng <zhangjinpeng@easy2study.net>
2017-12-19 18:00:26 +0800

    新增功能：添加管理员和站长权限

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -28,9 +28,5 @@ class AuthServiceProvider extends ServiceProvider
     {
         $this->registerPolicies();
 
-        \Horizon::auth(function ($request) {
-            // 是否是站长
-            return \Auth::user()->hasRole('Founder');
-        });
     }
 }
