d9311536c1aa0230d2aa4a5c9070f3f31d1dc31a
zhangjinpeng <zhangjinpeng@easy2study.net>
2017-12-20 14:21:49 +0800

    新增功能：管理员管理用户

--- config/administrator.php
+++ config/administrator.php
@@ -2,66 +2,39 @@
 
 return array(
 
-    /*
-     * 后台的 URI 入口
-     * Package URI
-     *
-     * @type string
-     */
+    // 后台的 URI 入口
     'uri' => 'admin',
 
-    /*
-     * 后台专属域名，没有的话可以留空
-     *  Domain for routing.
-     *
-     *  @type string
-     */
+    // 后台专属域名，没有的话可以留空
     'domain' => '',
 
-    /*
-     * 应用名称，在页面标题和左上角站点名称处显示
-     * Page title
-     *
-     * @type string
-     */
-    'title' => env('APP_NAME', 'Laravel'),
+    // 应用名称，在页面标题和左上角站点名称处显示
+    'title' => env('APP_NAME', 'laraBBS'),
 
-    /*
-     * 模型配置信息文件存放目录
-     * The path to your model config directory
-     *
-     * @type string
-     */
+    // 模型配置信息文件存放目录
     'model_config_path' => config_path('administrator'),
 
-    /*
-     * 配置信息文件存放目录
-     * The path to your settings config directory
-     *
-     * @type string
-     */
+    // 配置信息文件存放目录
     'settings_config_path' => config_path('administrator/settings'),
 
     /*
-     * The menu structure of the site. For models, you should either supply the name of a model config file or an array of names of model config
-     * files. The same applies to settings config files, except you must prepend 'settings.' to the settings config file name. You can also add
-     * custom pages by prepending a view path with 'page.'. By providing an array of names, you can group certain models or settings pages
-     * together. Each name needs to either have a config file in your model config path, settings config path with the same name, or a path to a
-     * fully-qualified Laravel view. So 'users' would require a 'users.php' file in your model config path, 'settings.site' would require a
-     * 'site.php' file in your settings config path, and 'page.foo.test' would require a 'test.php' or 'test.blade.php' file in a 'foo' directory
-     * inside your view directory.
+     * 后台菜单数组，多维数组渲染结果为多级嵌套菜单。
      *
-     * @type array
+     * 数组里的值有三种类型：
+     * 1. 字符串 —— 子菜单的入口，不可访问；
+     * 2. 模型配置文件 —— 访问 `model_config_path` 目录下的模型文件，如 `users` 访问的是 `users.php` 模型配置文件；
+     * 3. 配置信息 —— 必须使用前缀 `settings.`，对应 `settings_config_path` 目录下的文件，如：默认设置下，
+     *              `settings.site` 访问的是 `administrator/settings/site.php` 文件
+     * 4. 页面文件 —— 必须使用前缀 `page.`，如：`page.pages.analytics` 对应 `administrator/pages/analytics.php`
+     *               或者是 `administrator/pages/analytics.blade.php` ，两种后缀名皆可
      *
-     * 	array(
-     *		'E-Commerce' => array('collections', 'products', 'product_images', 'orders'),
-     *		'homepage_sliders',
-     *		'users',
-     *		'roles',
-     *		'colors',
-     *		'Settings' => array('settings.site', 'settings.ecommerce', 'settings.social'),
-     * 		'Analytics' => array('E-Commerce' => 'page.ecommerce.analytics'),
-     *	)
+     * 示例：
+     *  [
+     *      'users',
+     *      'E-Commerce' => ['collections', 'products', 'product_images', 'orders'],
+     *      'Settings'  => ['settings.site', 'settings.ecommerce', 'settings.social'],
+     *      'Analytics' => ['E-Commerce' => 'page.pages.analytics'],
+     *  ]
      */
     'menu' => [
         '用户与权限' => [
@@ -70,11 +43,11 @@ return array(
     ],
 
     /*
-    * 权限控制的回调函数。
-    *
-    * 此回调函数需要返回 true 或 false ，用来检测当前用户是否有权限访问后台。
-    * `true` 为通过，`false` 会将页面重定向到 `login_path` 选项定义的 URL 中。
-    */
+     * 权限控制的回调函数。
+     *
+     * 此回调函数需要返回 true 或 false ，用来检测当前用户是否有权限访问后台。
+     * `true` 为通过，`false` 会将页面重定向到 `login_path` 选项定义的 URL 中。
+     */
     'permission' => function () {
         // 只要是能管理内容的用户，就允许访问后台
         return Auth::check() && Auth::user()->can('manage_contents');
@@ -108,6 +81,4 @@ return array(
 
     // 可选的语言，如果不为空，将会在页面顶部显示『选择语言』按钮
     'locales' => [],
-
-//    'custom_routes_file' => app_path('Http/routes/administrator.php'),
 );
