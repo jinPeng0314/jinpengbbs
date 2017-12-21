7a3bbfa44b74e1384121bb4338195be25cc7aa2d
zhangjinpeng <zhangjinpeng@easy2study.net>
2017-12-21 11:33:36 +0800

    新增功能：设置论坛首页

--- routes/web.php
+++ routes/web.php
@@ -11,7 +11,7 @@
 |
 */
 
-Route::get('/', 'PagesController@root')->name('root');
+Route::get('/', 'TopicsController@index')->name('root');
 
 // 登录
 Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
