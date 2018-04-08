22793d0c5ecfac0d5a844399f3bda0c425000646
zhangjinpeng <zhangjinpeng@easy2study.net>
2018-04-08 22:57:42 +0800

    verificationCode store

---
+++ app/Http/Controllers/Api/VerificationCodesController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use Illuminate\Http\Request;
+use Overtrue\EasySms\EasySms;
+use App\Http\Requests\Api\VerificationCodeRequest;
+
+class VerificationCodesController extends Controller
+{
+    public function store(VerificationCodeRequest $request,EasySms $easySms)
+    {
+        $phone = $request->phone;
+
+        if (!app()->environment('production')) {
+            $code = '1234';
+        }else{
+            // 生成4位随机数，左侧补0
+            //str_pad函数使用  https://blog.csdn.net/baidu_34820252/article/details/51512748
+            $code = str_pad(random_int(1, 9999), 4, 0, STR_PAD_LEFT);
+
+            try {
+                $result = $easySms->send($phone, [
+                    'content'  =>  "【jpBBS社区】您的验证码是{$code}。如非本人操作，请忽略本短信"
+                ]);
+            } catch (\GuzzleHttp\Exception\ClientException $exception) {
+                $response = $exception->getResponse();
+                $result = json_decode($response->getBody()->getContents(), true);
+                return $this->response->errorInternal($result['msg'] ?? '短信发送异常');
+            }
+        }
+        //str_random函数通过指定长度生成随机字符串
+        $key = 'verificationCode_'.str_random(15);
+
+        $expiredAt = now()->addMinutes(10);
+        // 缓存验证码 10分钟过期。
+        \Cache::put($key, ['phone' => $phone, 'code' => $code], $expiredAt);
+
+        return $this->response->array([
+            'key' => $key,
+            'expired_at' => $expiredAt->toDateTimeString(),
+        ])->setStatusCode(201);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -2,17 +2,11 @@
 
 use Illuminate\Http\Request;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
-
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+ $api = app('Dingo\Api\Routing\Router');
+ $api->version('v1',[
+     'namespace' => 'App\Http\Controllers\Api'
+ ],function ($api){
+     //短信验证码
+     $api->post('verificationCodes','VerificationCodesController@store')
+         ->name('api.verificationCodes.store');
+ });
