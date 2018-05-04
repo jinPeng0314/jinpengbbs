eb2dd27f4eb4c5b45106433975c78857cc691a0d
zhangjinpeng <zhangjinpeng@easy2study.net>
2018-05-04 10:47:22 +0800

    某个用户发布的话题列表接口

--- app/Http/Controllers/Api/TopicsController.php
+++ app/Http/Controllers/Api/TopicsController.php
@@ -3,12 +3,38 @@
 namespace App\Http\Controllers\Api;
 
 use App\Models\Topic;
+use App\Models\User;
 use Illuminate\Http\Request;
 use App\Transformers\TopicTransformer;
 use App\Http\Requests\Api\TopicRequest;
 
 class TopicsController extends Controller
 {
+    public function index(Request $request,Topic $topic)
+    {
+        $query  = $topic->query(); //开始查询模型。
+
+        //分类条件查询
+        if ($categoryId = $request->category_id){
+            $query->where('category_id', $categoryId);
+        }
+
+        // 为了说明 N+1问题，不使用 scopeWithOrder
+        switch ($request->order) {
+            case 'recent':
+                $query->recent();
+                break;
+
+            default:
+                $query->recentReplied();
+                break;
+        }
+
+        $topics = $query->paginate(20);
+
+        return $this->response->paginator($topics, new TopicTransformer());
+    }
+
     public function store(TopicRequest $request,Topic $topic)
     {
         //用一组属性填充模型
@@ -33,5 +59,12 @@ class TopicsController extends Controller
         $topic->delete();
 
         return $this->response->noContent();
+    }
+
+    public function userIndex(Request $request,User $user)
+    {
+        $topics = $user->topics()->recent()->paginate(20);
+
+        return $this->response->paginator($topics,new TopicTransformer());
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -47,7 +47,11 @@ use Illuminate\Http\Request;
          // 游客可以访问的接口
          $api->get('categories', 'CategoriesController@index')
              ->name('api.categories.index');
-
+         $api->get('topics', 'TopicsController@index')
+             ->name('api.topics.index');
+         //某个用户发布的话题列表
+         $api->get('users/{user}/topics', 'TopicsController@userIndex')
+             ->name('api.users.topics.index');
 
          // 需要 token 验证的接口
          $api->group(['middleware' => 'api.auth'], function($api) {
