4f56cf8f20e6986165612e6e503af6499e9b8d34
zhangjinpeng <zhangjinpeng@easy2study.net>
2018-07-03 23:03:50 +0800

    add easyWeChat

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -3,6 +3,8 @@
 namespace App\Providers;
 
 use Illuminate\Support\Facades\Gate;
+use Carbon\Carbon;
+use Laravel\Passport\Passport;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 
 class AuthServiceProvider extends ServiceProvider
@@ -28,5 +30,16 @@ class AuthServiceProvider extends ServiceProvider
     {
         $this->registerPolicies();
 
+        // Passport 的路由
+        Passport::routes();
+        // access_token 过期时间
+        Passport::tokensExpireIn(Carbon::now()->addDays(15));
+        // refreshTokens 过期时间
+        Passport::refreshTokensExpireIn(Carbon::now()->addDays(30));
+
+//        \Horizon::auth(function ($request) {
+//            // 是否是站长
+//            return \Auth::user()->hasRole('Founder');
+//        });
     }
 }
